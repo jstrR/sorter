6171f9b9876a70b1e44d6de423679f1f856d9337
jstrR <srovk22@gmail.com>
2018-02-21 17:12:59 +0300

    did how much time i had

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.constr = [];
+    
   }
 
   add(element) {
     // your implementation
+    this.constr.push(element);  
   }
 
   at(index) {
     // your implementation
+    return this.constr[index];
   }
 
   get length() {
     // your implementation
+    return this.constr.length;
   }
 
   toArray() {
     // your implementation
+    return  this.constr;
   }
 
   sort(indices) {
     // your implementation
+    
   }
 
   setComparator(compareFunction) {
     // your implementation
+    
   }
 }
 
